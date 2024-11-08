cd7fe892ca1bb605a3c3483ccc230a6cd330a555
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-07 21:48:53 -0300

    criando rotas de listagem e criação de usuário

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 import http from "node:http"
 
 const server = http.createServer((req, res) => {
-  
+  const { method, url } = req
+  if(method === 'GET' && url === '/users') {
+    return res.end('listagem de usuários')
+  } 
+  if (method === 'POST' && url === '/users') {
+    return res.end('cadastro de usuários')
+  }
   return res.end('Hello World!')
 })
 
