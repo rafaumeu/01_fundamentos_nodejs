e1c34a1ecdab58c5e20bbb6f9c24fcad7d8210d2
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-11 08:07:21 -0300

    rotas com parâmetro validando com Regex

--- src/server.js
+++ src/server.js
@@ -9,9 +9,11 @@ import { routes } from "./routes.js"
 const server = http.createServer(async (req, res) => {
   await json(req, res)
   const { method, url } = req
-  const route = routes.find(route => route.method === method && route.path === url)
+  const route = routes.find(route => route.method === method && route.path.test(url))
 
   if (route) {
+    const routeParams = req.url.match(route.path)
+    console.log(routeParams)
     return route.handler(req, res)
   }
   return res.writeHead(404).end()
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,5 +1,6 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
-
-  console.log(Array.from(path.matchAll(routeParametersRegex)))
+  const pathWithParams = path.replace(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+  return pathRegex
 }
