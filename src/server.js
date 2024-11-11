78732e3ddf8d508f9cdb198721a2afb2d83f657d
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-11 04:23:26 -0300

    retornando status code nas rotas get e post
    adicionando segundos ao setTimeOut

--- src/server.js
+++ src/server.js
@@ -18,9 +18,9 @@ const server = http.createServer((req, res) => {
       name: "John Doe",
       email: "johndoe@example.com"
     })
-    return res.end('cadastro de usuários')
+    return res.writeHead(201).end()
   }
-  return res.end('Hello World!')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
